9f0bcf5e8662105b06fde479c8021e88ab0b2486
marmot <279099862@qq.com>
2017-08-25 15:38:12 +0800

    添加主页底部菜单布局。运用RadioGroup,RadioButton. 注意：（1）RadioGroup必须设置orientation, 否则会button不显示 （2）button的checked状态有默认设置时，需要设置ID，设置该按钮的id或者设置所有按钮的id （3）去掉RadioButton中的按钮图标，button="@null"

--- app/src/main/java/cn/raymond/www/iBasket/view/MainActivity.java
+++ app/src/main/java/cn/raymond/www/iBasket/view/MainActivity.java
@@ -2,9 +2,11 @@ package cn.raymond.www.iBasket.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 
 import cn.raymond.www.iBasket.R;
+import cn.raymond.www.iBasket.model.utils.MFGT;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onSkip(View view) {
+        MFGT.startActivity(MainActivity.this, TestActivity.class);// 左面进入，右面退出——整体右滑动
+    }
+
+    public void onClick(View view) {
+
     }
 }
